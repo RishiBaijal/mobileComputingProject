7d63825b5b3808d39dce1486b21f86ba75decf72
Rishi Baijal <rishi12084@iiitd.ac.in>
2014-10-27 23:09:40 +0530

    Update

--- src/com/example/flightsofangels/DisplayMessageActivity.java
+++ src/com/example/flightsofangels/DisplayMessageActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+//This is the file that I will be working on next
 public class DisplayMessageActivity extends ActionBarActivity {
 
 	@Override

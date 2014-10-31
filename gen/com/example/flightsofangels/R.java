f6f87dfcdb77dfffb69a62ef282cb7f153089855
Rishi Baijal <rishi12084@iiitd.ac.in>
2014-11-01 03:30:40 +0530

    Incorrect button ids are being passed to the OngoingPolls activity

--- gen/com/example/flightsofangels/R.java
+++ gen/com/example/flightsofangels/R.java
@@ -1104,14 +1104,16 @@ containing a value of this type.
         public static final int activity_display_message=0x7f030018;
         public static final int activity_join_poll=0x7f030019;
         public static final int activity_main=0x7f03001a;
-        public static final int activity_results_of_create_poll=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_ongoing_polls=0x7f03001b;
+        public static final int activity_results_of_create_poll=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int display_message=0x7f0c0000;
         public static final int join_poll=0x7f0c0001;
         public static final int main=0x7f0c0002;
-        public static final int results_of_create_poll=0x7f0c0003;
+        public static final int ongoing_polls=0x7f0c0003;
+        public static final int results_of_create_poll=0x7f0c0004;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1163,6 +1165,7 @@ containing a value of this type.
         public static final int submit_button=0x7f0a0015;
         public static final int title_activity_display_message=0x7f0a0012;
         public static final int title_activity_join_poll=0x7f0a0013;
+        public static final int title_activity_ongoing_polls=0x7f0a0017;
         public static final int title_activity_results_of_create_poll=0x7f0a0014;
     }
     public static final class style {
--- src/com/example/flightsofangels/JoinPollActivity.java
+++ src/com/example/flightsofangels/JoinPollActivity.java
@@ -2,15 +2,24 @@ package com.example.flightsofangels;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class JoinPollActivity extends ActionBarActivity {
+
+	protected static String s;
+	Button radio;
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,39 @@ public class JoinPollActivity extends ActionBarActivity {
 		// int i;
 		// String s = "";
 		// final LinearLayout lm=(LinearLayout) findViewById(R.id.buttonlayout);
-		 ArrayList<String> arr = getPollsInNetwork();
-		 addRadioButtons(arr.size(), "Radio button #");
+		ArrayList<String> arr = getPollsInNetwork();
+		int number = arr.size();
+		int i, row;
+		//String s;
+		for (row = 0; row < 1; row++) {
+			LinearLayout ll = new LinearLayout(this);
+			ll.setOrientation(LinearLayout.VERTICAL);
+			final Intent intent = new Intent(this, OngoingPolls.class);
+			for (i = 1; i <= number; i++) {
+				radio = new Button(this);
+				radio.setId(row * 2 + i);
+				radio.setText("Poll # " + radio.getId());
+				radio.setOnClickListener(new OnClickListener(){
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						JoinPollActivity.s=""+radio.getId();
+						intent.putExtra(EXTRA_MESSAGE, s);
+						startActivity(intent);
+					}
+					
+					
+				});
+				ll.addView(radio);
+			}
+
+			((ViewGroup) findViewById(R.id.radiogroup)).addView(ll);
+
+		}
+
+		// addRadioButtons(arr.size(), "Radio button #");
+
 		// for (i = 0; i < arr.size(); i++) {
 		/*
 		 * Button button=new Button(this); s+=arr.get(i)+"\n";
@@ -67,23 +107,33 @@ public class JoinPollActivity extends ActionBarActivity {
 
 	}
 
-	public void addRadioButtons(int number, String text) {
-		int i, row;
-		for (row = 0; row < 1; row++) {
-			LinearLayout ll = new LinearLayout(this);
-			ll.setOrientation(LinearLayout.VERTICAL);
-			 for (i=1; i<=number; i++)
-			 {
-				 RadioButton radio=new RadioButton(this);
-				 radio.setId(row*2 + i);
-				 radio.setText(text + radio.getId()); 
-				 ll.addView(radio);
-			 }
-			 
-			 ((ViewGroup) findViewById(R.id.radiogroup)).addView(ll);
-			
-		}
+	// public void addRadioButtons(int number, String text) {
+	// int i, row;
+	// for (row = 0; row < 1; row++) {
+	// LinearLayout ll = new LinearLayout(this);
+	// ll.setOrientation(LinearLayout.VERTICAL);
+	// for (i=1; i<=number; i++)
+	// {
+	// radio=new RadioButton(this);
+	// radio.setId(row*2 + i);
+	// radio.setText(text + radio.getId());
+	// ll.addView(radio);
+	// }
+	//
+	// ((ViewGroup) findViewById(R.id.radiogroup)).addView(ll);
+	//
+	// }
+	//
+	// }
 
+	public void submitted(View view) {
+		RadioGroup rg = (RadioGroup) findViewById(R.id.radiogroup);
+		// Button next=(Button) findViewById(R.id.btn);
+		// next.set
+		String s = "" + rg.getCheckedRadioButtonId();
+		Intent intent = new Intent(this, OngoingPolls.class);
+		intent.putExtra(EXTRA_MESSAGE, s);
+		startActivity(intent);
 	}
 
 	@Override
@@ -97,7 +147,7 @@ public class JoinPollActivity extends ActionBarActivity {
 		int x = 4;
 		/*
 		 * This function will iterate over the list of people who are
-		 * regsitered, and print the results
+		 * registered, and print the results
 		 */
 		return x;
 	}
